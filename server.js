3353b5cf5ede4285a4cefb146c00a01a6ff9485b
gabrielqap <gabrielqap@hotmail.com>
2020-07-19 00:18:52 -0300

    Feito o CRUD da API.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Inicia app
+const app = express();
+app.use(express.json());
+
+// Inicia DB
+mongoose.connect('mongodb://localhost:27017/usersapi', 
+{useNewUrlParser: true, useUnifiedTopology : true});
+
+requireDir('./src/models/');
+
+
+app.use('/api', require('./src/routes'));
+app.listen(3001);
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const express  = require('express');
+const routes = express.Router();
+const UserController = require('./controllers/UserController');
+
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.remove);
+
+module.exports = routes;
